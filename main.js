0e3b20c661fbb8bea8b16ec70e1b2d17f05e50fb
KB Choi <1mdifferent@naver.com>
2024-02-23 03:03:04 +0900

    make side menu

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const API_KEY = "b6514b7a92d34fa18407c14d9fd7ad2c";
 let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
 const getLatestNews = async () => {
   // const url = new URL(
   //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
@@ -13,6 +18,32 @@ const getLatestNews = async () => {
   render();
   console.log("dddd", newsList);
 };
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category", category);
+  const url = new URL(
+    `https://nimble-semolina-90d596.netlify.app/top-headlines?country=kr&category=${category}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("dddd", data);
+  newsList = data.articles;
+  render();
+};
+
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById("search-input").value;
+  console.log("keyword", keyword);
+  const url = new URL(
+    `https://nimble-semolina-90d596.netlify.app/top-headlines?country=kr&q=${keyword}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+};
+
 const render = () => {
   const newsHTML = newsList
     .map(
@@ -33,3 +64,11 @@ const render = () => {
 };
 
 getLatestNews();
+
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
